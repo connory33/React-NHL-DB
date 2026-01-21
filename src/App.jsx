70628bbfb1be1ddd4c6ae8a8f34a98cf6ee19a84
connory33 <connor@connoryoung.com>
2026-01-20 21:55:16 -0500

    Test deployment with separate secrets

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <h1>React NHL Database</h1>
-      {/* Deployment test */}
+      {/* Testing deployment with correct secrets */}
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
